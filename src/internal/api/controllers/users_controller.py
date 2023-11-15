653aa233bfd7961757bfb140f347e7206d445462
Sami <sami.perrin@chuv.ch>
2023-11-15 10:34:12 +0000

    add user service migration, proto and controller

---
+++ src/internal/api/controllers/users_controller.py
@@ -0,0 +1,90 @@
+import connexion
+from typing import Dict
+from typing import Tuple
+from typing import Union
+from inspect import getmembers, isfunction
+
+
+from server_template.models.rpc_status import RpcStatus
+from server_template.models.templatebackend_user import TemplatebackendUser
+from server_template.models.templatebackend_create_user_reply import TemplatebackendCreateUserReply
+from server_template.models.templatebackend_create_user_result import TemplatebackendCreateUserResult
+# from server_template.models.templatebackend_ import 
+# from server_template.models.templatebackend_ import 
+from server_template.models.templatebackend_update_password_request import TemplatebackendUpdatePasswordRequest
+from server_template import util
+
+def user_service_create_user(body: TemplatebackendUser):
+    return TemplatebackendCreateUserReply(TemplatebackendCreateUserResult(id=1))
+
+
+def user_service_delete_user(id: str):
+    """Delete a user
+
+    This endpoint deletes a user
+
+    :param id: 
+    :type id: str
+
+    :rtype: Union[TemplatebackendDeleteUserReply, Tuple[TemplatebackendDeleteUserReply, int], Tuple[TemplatebackendDeleteUserReply, int, Dict[str, str]]
+    """
+
+    return None
+
+
+def user_service_get_user(id: str):
+    """Get a user
+
+    This endpoint returns a user
+
+    :param id: 
+    :type id: str
+
+    :rtype: Union[TemplatebackendGetUserReply, Tuple[TemplatebackendGetUserReply, int], Tuple[TemplatebackendGetUserReply, int, Dict[str, str]]
+    """
+
+    return None
+
+
+def user_service_get_user_me():
+    """Get my own user
+
+    This endpoint returns the details of the authenticated user
+
+
+    :rtype: Union[TemplatebackendGetUserMeReply, Tuple[TemplatebackendGetUserMeReply, int], Tuple[TemplatebackendGetUserMeReply, int, Dict[str, str]]
+    """
+
+    return None
+
+
+def user_service_reset_password(id: str, body: object):
+    """Reset password
+
+    This endpoint resets a user&#39;s password
+
+    :param id: 
+    :type id: str
+    :param body: 
+    :type body: 
+
+    :rtype: Union[TemplatebackendResetPasswordReply, Tuple[TemplatebackendResetPasswordReply, int], Tuple[TemplatebackendResetPasswordReply, int, Dict[str, str]]
+    """
+
+    return None
+
+
+def user_service_update_password(body: TemplatebackendUpdatePasswordRequest):
+    """Update password
+
+    This endpoint updates the password of the authenticated user
+
+    :param body: 
+    :type body: dict | bytes
+
+    :rtype: Union[TemplatebackendUpdatePasswordReply, Tuple[TemplatebackendUpdatePasswordReply, int], Tuple[TemplatebackendUpdatePasswordReply, int, Dict[str, str]]
+    """
+    # if connexion.request.is_json:
+    #     body = TemplatebackendUpdatePasswordRequest.from_dict(connexion.request.get_json())
+
+    return None
--- src/internal/migrations/postgresql/0001-initial-migrations.sql
+++ src/internal/migrations/postgresql/0001-initial-migrations.sql
@@ -1,4 +1,36 @@
-CREATE TABLE IF NOT EXISTS hello
-(
-	id SERIAL PRIMARY KEY
+CREATE TABLE IF NOT EXISTS users (
+    id SERIAL PRIMARY KEY,
+	-- tenantid INT64 NULL,
+
+    username  TEXT UNIQUE,
+    password  TEXT,
+    firstname TEXT,
+    lastname  TEXT,
+    status    TEXT,
+	source	  TEXT,
+
+	totpsecret TEXT,
+
+    createdat TIMESTAMP,
+    updatedat TIMESTAMP
+
+	-- CONSTRAINT tenantcon FOREIGN KEY (tenantid) REFERENCES tenants(id)
 );
+
+CREATE TABLE IF NOT EXISTS roles (
+	id TEXT PRIMARY KEY
+);
+
+
+CREATE TABLE IF NOT EXISTS user_role (
+	id SERIAL PRIMARY KEY,
+
+    userid INT64,
+	role TEXT,
+
+	CONSTRAINT usercon FOREIGN KEY (userid) REFERENCES users(id),
+	CONSTRAINT rolecon FOREIGN KEY (role) REFERENCES roles(id)
+);
+
+insert into roles (name) select 'admin' where not exists (select * from roles where name = 'admin');
+insert into roles (name) select 'daemon' where not exists (select * from roles where name = 'daemon');
