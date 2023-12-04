a262b21160d63118d7a564d51ce635ccac7b002b
Sami <sami.perrin@chuv.ch>
2023-12-04 17:24:35 +0100

    update deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -7,5 +7,7 @@ set -e
 
 mkdir -p ./chart/files
 cp -r ../configs/$env/* ./chart/files/
+echo template
 helm template --namespace ci --values ./chart/files/values.yaml --set-string aesPassphrase=$CONFIG_AES_PASSPHRASE --set-string image=registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG} ./chart
+echo install
 helm install --create-namespace --namespace ci --values ./chart/files/values.yaml --set-string aesPassphrase=$CONFIG_AES_PASSPHRASE --set-string image=registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG} ./chart
