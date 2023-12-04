de36a9ccd9b68b79cf24ce38435de4d575f4d5d6
Sami <sami.perrin@chuv.ch>
2023-12-04 16:20:42 +0100

    add pull option to build

--- docker/build-stage1.sh
+++ docker/build-stage1.sh
@@ -1,2 +1,2 @@
-docker build -f dockerfiles/stage1.dockerfile -t registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend-stage1 --secret id=PYPI_USERNAME,env=PYPI_USERNAME --secret id=PYPI_PASSWORD,env=PYPI_PASSWORD ..
+docker build --pull -f dockerfiles/stage1.dockerfile -t registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend-stage1 --secret id=PYPI_USERNAME,env=PYPI_USERNAME --secret id=PYPI_PASSWORD,env=PYPI_PASSWORD ..
 docker push registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend-stage1
