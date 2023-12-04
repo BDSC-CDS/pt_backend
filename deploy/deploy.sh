ed4602579d6300539f52befb60aa8172d0f47795
Sami <sami.perrin@chuv.ch>
2023-12-04 16:48:12 +0100

    add missing dir

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -5,5 +5,6 @@ set -e
 # kubectl apply --force -f ./all.yaml
 # kubectl rollout restart -f ./all.yaml || true
 
+mkdir -p ./chart/files
 cp -r ../configs/$env/* ./chart/files/
 helm install --create-namespace --namespace ci --values ./chart/files/values.yaml --set-string aesPassphrase=$CONFIG_AES_PASSPHRASE --set-string image=registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG}
