d22de2551974bdd60058b4dff6692afbda0965a1
Sami <sami.perrin@chuv.ch>
2023-12-04 16:42:26 +0100

    deploy fail fast, install helm

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,5 +1,9 @@
+#!/bin/bash
+
+set -e
+
 # kubectl apply --force -f ./all.yaml
 # kubectl rollout restart -f ./all.yaml || true
 
-cp -r ../config/$env/* ./chart/files/
+cp -r ../configs/$env/* ./chart/files/
 helm install --create-namespace --namespace ci --values ./chart/files/values.yaml --set-string aesPassphrase=$CONFIG_AES_PASSPHRASE --set-string image=registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG}
