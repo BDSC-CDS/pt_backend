91d7b2a2412d72b4ec77c3a2ad80296d0cdfff4f
Sami <sami.perrin@chuv.ch>
2023-12-04 15:57:43 +0100

    pass pypi secret

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -1,4 +1,4 @@
-docker build -f dockerfiles/stage2.dockerfile -t registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG} ..
+docker build -f dockerfiles/stage2.dockerfile -t registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG} --secret id=PYPI_USERNAME,env=PYPI_USERNAME --secret id=PYPI_PASSWORD,env=PYPI_PASSWORD ..
 docker tag registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG} registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:latest
 docker push registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:${IMAGE_TAG}
 docker push registry.itrcs3-app.intranet.chuv/ds-cicd-template-backend:latest
