84a57229c3230766890fbd2c5d28469b8e533427
Sami <sami.perrin@chuv.ch>
2023-12-14 13:02:30 +0100

    plug test

--- src/tests/acceptance/user/test_user.py
+++ src/tests/acceptance/user/test_user.py
@@ -1,30 +1,11 @@
 import unittest
-import time
-import os
-import sys
-import traceback
-
-### Allow dynamic import resolution from generated template backend
-# Get the absolute path to src/internal/api/
-module_path = os.path.abspath(os.path.join('src', 'tests', 'client'))
-# Add this path to sys.path if it's not already there
-if module_path not in sys.path:
-    sys.path.append(module_path)
-
 import openapi_client
-from openapi_client.models.templatebackend_create_user_reply import TemplatebackendCreateUserReply
-from openapi_client.models.templatebackend_user import TemplatebackendUser
-from openapi_client.rest import ApiException
 from pprint import pprint
-
+from src.tests.acceptance.helpers.client import client
 
 class TestUser(unittest.TestCase):
-
     def test_create_user(self):
-        configuration = openapi_client.Configuration(
-            host = "http://127.0.0.1:5000"
-        )
-        with openapi_client.ApiClient(configuration) as api_client:
+        with client() as api_client:
             # Create an instance of the API class
             api_instance = openapi_client.UsersApi(api_client)
             body = openapi_client.TemplatebackendUser(
