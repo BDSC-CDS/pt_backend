3b5f65ba623ed2d185589f49f14c3c8e8f42790d
Sami <sami.perrin@chuv.ch>
2024-01-24 11:22:34 +0100

    add acceptance test for user

--- src/tests/acceptance/user/test_user.py
+++ src/tests/acceptance/user/test_user.py
@@ -4,7 +4,7 @@ from pprint import pprint
 from src.tests.acceptance.helpers.client import client
 
 class TestUser(unittest.TestCase):
-    def test_create_user(self):
+    def test_create_get_user(self):
         with client() as api_client:
             # Create an instance of the API class
             api_instance = openapi_client.UsersApi(api_client)
@@ -19,11 +19,24 @@ class TestUser(unittest.TestCase):
             print("The response of UsersApi->user_service_create_user:\n")
             pprint(api_response)
 
-        self.assertIsNotNone(api_response)
-        self.assertIsInstance(api_response.result.id, str)
-        id = int(api_response.result.id)
-        self.assertIsInstance(id, int)
-        self.assertTrue(id > 0)
+            self.assertIsNotNone(api_response)
+            self.assertIsInstance(api_response.result.id, str)
+            id = int(api_response.result.id)
+            self.assertIsInstance(id, int)
+            self.assertTrue(id > 0)
+
+            api_response = api_instance.user_service_get_user(id=id)
+            print("The response of UsersApi->user_service_get_user:\n")
+            pprint(api_response)
+            self.assertIsNotNone(api_response)
+            self.assertIsInstance(api_response.result.id, str)
+            self.assertEqual(id, api_response.result.id)
+
+            self.assertEqual("", api_response.result.password)
+            self.assertEqual("moto2", api_response.result.username)
+            self.assertEqual("hello.moto@gmail.com", api_response.result.email)
+            self.assertEqual("hello", api_response.result.firstName)
+            self.assertEqual("moto", api_response.result.lastName)
 
 if __name__ == '__main__':
     unittest.main()
