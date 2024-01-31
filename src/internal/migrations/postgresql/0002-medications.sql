1d7d4b84114cd344036b4d34b567fd2212114a32
Sami <sami.perrin@chuv.ch>
2024-01-31 14:49:08 +0100

    add medication migration

---
+++ src/internal/migrations/postgresql/0002-medications.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS medication (
+	id SERIAL PRIMARY KEY,
+	tenantid INT NOT NULL,
+
+    userid INT NOT NULL,
+	name TEXT NOT NULL,
+    dosage TEXT NOT NULL,
+    frequency TEXT NOT NULL,
+
+    createdat TIMESTAMP NOT NULL DEFAULT now(),
+    updatedat TIMESTAMP NOT NULL DEFAULT now(),
+    deletedat TIMESTAMP,
+
+	CONSTRAINT usercon FOREIGN KEY (userid) REFERENCES users(id)
+);
