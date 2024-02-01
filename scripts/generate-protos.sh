6e8f672c491764b43bff0b2f99f4805ab50dab40
Sami <sami.perrin@chuv.ch>
2024-02-01 10:06:34 +0100

    update initial migrations, add service level proto converter

--- scripts/generate-protos.sh
+++ scripts/generate-protos.sh
@@ -17,25 +17,26 @@ function generate_api_files() {
 
     mkdir -p api/openapiv2/v1-tags
 
-    # for file in api/proto/v1/*.proto; do
-    #     if [[ -f $file ]]; then
-    #         echo "---> generating grpc files..."
-    #         echo $(basename $file)
-    #         echo $file
-    #         #python3 -m grpc_tools.protoc --proto_path=. --proto_path=api/proto/third_party --python_out=internal --grpc_python_out=internal $file
-    #         # protoc --proto_path=api/proto/v1/ --proto_path=api/proto/third_party --go_out=plugins=grpc:internal/api/v1/templatebackend $(basename $file)
+    for file in api/proto/v1/*.proto; do
+        if [[ -f $file ]]; then
+            echo "---> generating grpc files..."
+            echo $(basename $file)
+            echo $file
+            #python3 -m grpc_tools.protoc --proto_path=. --proto_path=api/proto/third_party --python_out=internal --grpc_python_out=internal $file
+            # protoc --proto_path=api/proto/v1/ --proto_path=api/proto/third_party --go_out=plugins=grpc:internal/api/v1/templatebackend $(basename $file)
 
 
-    #         # echo "---> generating grpc gateway files..."
-    #         # protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --grpc-gateway_out=logtostderr=true:internal/api/v1/templatebackend $(basename $file)
+            # echo "---> generating grpc gateway files..."
+            # protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --grpc-gateway_out=logtostderr=true:internal/api/v1/templatebackend $(basename $file)
 
-    #         #echo "---> generating grpc validation files..."
-    #         #protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --govalidators_out=internal/api/v1/templatebackend  `basename $file`
+            #echo "---> generating grpc validation files..."
+            #protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --govalidators_out=internal/api/v1/templatebackend  `basename $file`
 
-    #         echo "---> generating openapiv2 files..."
-    #         protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --openapiv2_out=disable_default_errors=true,simple_operation_ids=true,logtostderr=true:api/openapiv2/v1-tags $(basename $file)
-    #     fi
-    # done
+            echo "---> generating openapiv2 files..."
+            # protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --openapiv2_out=disable_default_errors=true,simple_operation_ids=true,logtostderr=true:api/openapiv2/v1-tags $(basename $file)
+            protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --openapiv2_out=logtostderr=true,output_format=yaml:api/openapiv2/services $(basename $file)
+        fi
+    done
 
     echo "---> generating merged openapiv2 API definition file 'apis.openapiv2.json' ..."
     protoc --proto_path=api/proto/v1 --proto_path=api/proto/third_party --openapiv2_out=logtostderr=true,allow_merge=true,output_format=yaml,merge_file_name=apis:api/openapiv2/v1-tags api/proto/v1/*.proto
