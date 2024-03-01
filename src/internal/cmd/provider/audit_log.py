dedaf38cb6440725c72e1d46ad225e1d2e0ebd08
pcito <paloma.cito@chuv.ch>
2024-03-01 15:45:16 +0100

    provider for audit log

---
+++ src/internal/cmd/provider/audit_log.py
@@ -0,0 +1,33 @@
+from src.pkg.audit_log.service.audit_log import AuditLogService
+from src.pkg.audit_log.store.postgres import AuditLogStore as PostgresAuditStore
+from .db import provide_db_type, provide_db
+
+audit_log_service = None
+audit_log_store = None
+
+
+def provide_audit_log_service():
+    global audit_log_service
+
+    if audit_log_service is not None:
+        return audit_log_service
+
+    audit_log_service = AuditLogService(provide_audit_log_store())
+
+    return audit_log_service
+
+def provide_audit_log_store():
+    global audit_log_store
+
+    if audit_log_store is not None:
+        return audit_log_store
+
+    tpe = provide_db_type()
+
+    if tpe == "postgres":
+        audit_log_store = PostgresAuditStore(provide_db())
+    else:
+        raise NotImplementedError("datastore type " + tpe + " is not implemented")
+
+
+    return audit_log_store
