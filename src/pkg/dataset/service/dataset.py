30c9f2af73c03fa711bdb0f9f60bdfaec735bccb
pcito <paloma.cito@chuv.ch>
2024-06-10 15:56:32 +0200

    dataset service

--- src/pkg/dataset/service/dataset.py
+++ src/pkg/dataset/service/dataset.py
@@ -5,8 +5,8 @@ class DatasetService:
         self.dataset_store = dataset_store
 
     # store new dataset (store in metadata, dataset,values)
-    def store_dataset(self, dataset: Dataset): # TODO update csv?
-        self.dataset_store.store_dataset(Dataset)
+    def store_dataset(self, path:str): # TODO update csv?
+        self.dataset_store.store_dataset(path)
 
     # get names of datasets related to this user (if not deleted)
     def get_list_datasets(self, identifier:int, tenantid:int, offset:int,limit:int):
@@ -19,19 +19,19 @@ class DatasetService:
         return dataset
 
     # get dataset (if not deleted)
-    def get_dataset(self,name:str, identifier:int, tenantid:int, offset:int,limit:int):
-        dataset = self.dataset_store.get_dataset(name=name,identifier=identifier,tenantid=tenantid, offset=offset,limit=limit)
+    def get_dataset_content(self,name:str, identifier:int, tenantid:int, offset:int,limit:int):
+        dataset = self.dataset_store.get_dataset_content(name=name,identifier=identifier,tenantid=tenantid, offset=offset,limit=limit)
         return dataset
 
-    # TODO how would we modify the metadata
-    def update_metadata_dataset(self,name:str,identifier:int, tenantid:int,metadata:str):
-        updated = self.dataset_store.update_metadata_dataset(name=name, identifier=identifier, tenantid=tenantid,metadata=metadata)
-        return updated
+    # # TODO how would we modify the metadata
+    # def update_metadata_dataset(self,name:str,identifier:int, tenantid:int,metadata:str):
+    #     updated = self.dataset_store.update_metadata_dataset(name=name, identifier=identifier, tenantid=tenantid,metadata=metadata)
+    #     return updated
 
-    # TODO just replace dataset?
-    def update_dataset(self,name:str,identifier:int, tenantid:int,dataset: Dataset):
-        updated = self.dataset_store.update_dataset(name=name, identifier=identifier, tenantid=tenantid,dataset=dataset)
-        return updated
+    # # TODO just replace dataset?
+    # def update_dataset(self,name:str,identifier:int, tenantid:int,dataset: Dataset):
+    #     updated = self.dataset_store.update_dataset(name=name, identifier=identifier, tenantid=tenantid,dataset=dataset)
+    #     return updated
 
     # delete dataset (just update deleted_at, don't actually delete it)
     def delete_dataset(self,name:str,identifier:int, tenantid:int):
