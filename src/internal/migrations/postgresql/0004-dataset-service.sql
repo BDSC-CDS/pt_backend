90d103c98aff3c90d71a0313dd8af7663fda4aad
pcito <paloma.cito@chuv.ch>
2024-06-10 15:42:25 +0200

    migration

--- src/internal/migrations/postgresql/0004-dataset-service.sql
+++ src/internal/migrations/postgresql/0004-dataset-service.sql
@@ -1,12 +1,36 @@
-CREATE TABLE IF NOT EXISTS datasets (
+ CREATE TABLE IF NOT EXISTS datasets (
     id SERIAL PRIMARY KEY,
     userid INT,
-    name TEXT,
-    data JSONB,
+    tenantid INT,
+    dataset_name TEXT,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT dataset_fk FOREIGN KEY (userid) REFERENCES users(id)
+    deleted_at TIMESTAMP,
+    CONSTRAINT datasets_fk FOREIGN KEY (userid) REFERENCES users(id)
 );
 
+CREATE INDEX idx_datasets_userid_createdat ON datasets (userid, created_at);
 
-CREATE INDEX idx_dataset_userid_createdat ON datasets (userid, created_at);
+CREATE TABLE IF NOT EXISTS metadata (
+    userid INTEGER,
+    tenantid INTEGER,
+    dataset_id INTEGER,
+    column_id INTEGER,
+    type_ TEXT,
+    PRIMARY KEY (dataset_id, column_id),
+    CONSTRAINT metadata_fk FOREIGN KEY (dataset_id) REFERENCES datasets(id)
+    CONSTRAINT metadata_fk FOREIGN KEY (userid) REFERENCES users(id)
+);
+CREATE INDEX idx_metadata_dataset_id_userid ON metadata (userid, dataset_id, column_id);
+
+CREATE TABLE IF NOT EXISTS dataset_content (
+    userid INTEGER,
+    tenantid INTEGER,
+    dataset_id INTEGER,
+    column_id INTEGER,
+    line_id INTEGER,
+    val TEXT,
+    PRIMARY KEY (dataset_id, column_id, line_),
+    CONSTRAINT dataset_content_fk FOREIGN KEY (dataset_id) REFERENCES datasets(id)
+    CONSTRAINT dataset_content_fk FOREIGN KEY (userid) REFERENCES users(id)
+);
+CREATE INDEX idx_dataset_content_dataset_id_userid ON dataset_content (userid, dataset_id, column_id,line_id);
