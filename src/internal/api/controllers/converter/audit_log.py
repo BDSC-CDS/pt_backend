cb61ae7478e164ed8175f7b3d2df2b5ad8e1ce11
kobikacc <148439855+kobikacc@users.noreply.github.com>
2024-06-27 11:12:28 +0000

    complete converter

--- src/internal/api/controllers/converter/audit_log.py
+++ src/internal/api/controllers/converter/audit_log.py
@@ -5,14 +5,22 @@ def audit_log_to_business(audit_log: TemplatebackendAuditLog) -> AuditLog:
     return AuditLog(
         id=audit_log.id,
         userid=audit_log.user_id,
-        #TODO complete all fields
+        service=audit_log.service,
+        action=audit_log.action,
+        body=audit_log.body,
+        response=audit_log.response,
+        created_at=audit_log.created_at,
+        error=audit_log.error
     )
 
 def audit_log_from_business(audit_log: AuditLog) -> TemplatebackendAuditLog:
     return TemplatebackendAuditLog(
         id=audit_log.id,
         user_id=audit_log.userid,
-        #TODO complete all fields
+        service=audit_log.service,
+        action=audit_log.action,
+        body=audit_log.body,
+        response=audit_log.response,
         created_at=audit_log.createdat,
         updated_at=audit_log.updatedat
     )
