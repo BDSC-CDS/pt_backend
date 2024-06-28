af65136189237b7f6123d364fd2d2b8ccb335011
pcito <paloma.cito@chuv.ch>
2024-06-28 16:38:09 +0200

    removed constraint

--- src/internal/migrations/postgresql/0006-config-add-columns.sql
+++ src/internal/migrations/postgresql/0006-config-add-columns.sql
@@ -2,5 +2,5 @@ ALTER TABLE config_generator
 ADD COLUMN userid INT,
 ADD COLUMN tenantid INT;
 
-ALTER TABLE config_generator
-ADD CONSTRAINT config_generator_fk FOREIGN KEY (userid) REFERENCES users(id);
+-- ALTER TABLE config_generator
+-- ADD CONSTRAINT config_generator_fk FOREIGN KEY (userid) REFERENCES users(id);
