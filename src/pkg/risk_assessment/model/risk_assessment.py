1de8d76377ba768e808fc397074f973c5f8c27a6
bkaabachi <mohamed-beyrem.kaabachi@chuv.ch>
2024-12-11 11:03:55 +0000

    add risk assessment data class

--- src/pkg/risk_assessment/model/risk_assessment.py
+++ src/pkg/risk_assessment/model/risk_assessment.py
@@ -3,6 +3,9 @@ from dataclasses import dataclass, asdict
 @dataclass
 class RiskAssessment:
     userid: int = None
+    dataset_id: int = None
+    tenantid: int = None
+    risk_assessment: str = ""
 
     def to_dict(self):
         return asdict(self)
