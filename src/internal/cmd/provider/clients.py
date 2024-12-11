961df0396b394ba2a04cc2776fa0e88df5688d29
bkaabachi <mohamed-beyrem.kaabachi@chuv.ch>
2024-12-11 11:57:52 +0000

    change low level logic from service to client

--- src/internal/cmd/provider/clients.py
+++ src/internal/cmd/provider/clients.py
@@ -1,16 +1,60 @@
-
-
+from python_arx_deidentifier.client.assessment_client import apply_assessment
+from python_arx_deidentifier.client.configuration_client import (
+    build_basic_configuration,
+    set_data_as_csv,
+    set_nb_solutions,
+    set_suppression_limit,
+    set_is_used_population_model,
+    configure_attribute,
+)
 from src.internal.clients.arx import client
 from .config import provide_config
+import pandas as pd 
 
 arx_client = None
 
 def provide_arx_client():
     global arx_client
-
     if arx_client is not None:
         return arx_client
 
     arx_client = client.ArxClient(provide_config().clients.arx)
-    
     return arx_client
+
+def build_config_from_dict(json_config: dict):
+    """
+    Builds an ARX configuration object from a dictionary.
+
+    Args:
+        json_config (dict): Configuration data for ARX.
+
+    Returns:
+        Configured ARX configuration object.
+    """
+    arx_config = build_basic_configuration()
+
+    for attr in json_config["attributes"]:
+        arx_config = configure_attribute(
+            arx_config,
+            attr["key"],
+            attr.get("source"),
+            attr["type"],
+            attr.get("weight"),
+        )
+    return arx_config
+
+def perform_risk_analysis(dataset: pd.DataFrame, json_config: dict, arx_client):
+    """
+    Performs risk analysis on the dataset.
+
+    Args:
+        dataset (pd.DataFrame): Dataset to analyze.
+        json_config (dict): Configuration for ARX.
+        arx_client: ARX client instance.
+
+    Returns:
+        The result of the risk analysis.
+    """
+    arx_config = build_config_from_dict(json_config)
+    arx_config = set_data_as_csv(arx_config, dataset)
+    return apply_assessment(arx_client, arx_config)
--- src/pkg/risk_assessment/service/risk_assessment.py
+++ src/pkg/risk_assessment/service/risk_assessment.py
@@ -1,129 +1,18 @@
-import pandas as pd
-from python_arx_deidentifier.client.assessment_client import apply_assessment
-from python_arx_deidentifier.client.configuration_client import (
-    build_basic_configuration,
-    set_data_as_csv,
-    set_nb_solutions,
-    set_suppression_limit,
-    set_is_used_population_model,
-    configure_attribute,
-)
 from src.pkg.dataset.store.postgres import get_dataset_content
 from src.pkg.risk_assessment.model.risk_assessment import RiskAssessment
-
+from src.clients.arx.client import provide_arx_client, perform_risk_analysis
 
 class RiskAssessmentService:
-    """
-    Service to manage risk assessment using ARX de-identification client.
-    
-    This class handles the configuration of ARX risk assessment based on user-provided
-    settings and performs risk analysis on a specified dataset.
-    """
-
-    # TODO what do I do with the arx_client here? it's supposed to be the domain
-    # TODO 
-    def __init__(self, arx_client: str, dataset_id: int, tenantid: int, userid: int, risk_assessment_store):
-        """
-        Initializes the RiskAssessmentService.
-
-        Args:
-            arx_client: The ARX de-identification client instance.
-            dataset_id (int): ID of the dataset to be analyzed.
-            tenantid (int): ID of the tenant for multitenancy support.
-            userid (int): ID of the user initiating the analysis.
-        """
-        self.arx_client = arx_client
+    def __init__(self, dataset_id: int, tenantid: int, userid: int, risk_assessment_store):
         self.dataset_id = dataset_id
         self.tenantid = tenantid
         self.userid = userid
         self.risk_assessment_store = risk_assessment_store
-        
-# TODO add store and list
-
-
-    @staticmethod
-    def populate_config_from_dict(json_config: dict):
-        """
-        Builds and populates the ARX configuration object from a JSON-like dictionary.
-
-        Args:
-            json_config (dict): Configuration data including general settings and attribute details.
-                Expected format:
-                {
-                    "nb_solutions": int,
-                    "suppression_limit": float,
-                    "is_used_population_model": bool,
-                    "attributes": [
-                        {
-                            "key": str,
-                            "source": str or None,
-                            "type": str,
-                            "weight": float or None
-                        },
-                        ...
-                    ]
-                }
-
-        Returns:
-            arx_config: Configured ARX configuration object.
-        """
-        arx_config = build_basic_configuration()
-
-        # Set general configurations
-        arx_config = set_nb_solutions(arx_config, json_config["nb_solutions"])
-        arx_config = set_suppression_limit(arx_config, json_config["suppression_limit"])
-        arx_config = set_is_used_population_model(arx_config, json_config["is_used_population_model"])
-
-        # Configure individual attributes
-        for attr in json_config["attributes"]:
-            arx_config = configure_attribute(
-                arx_config,
-                attr["key"],
-                attr.get("source"),
-                attr["type"],
-                attr.get("weight"),
-            )
-
-        return arx_config
+        self.arx_client = provide_arx_client()
 
     def get_risk_assessment(self, json_config: dict) -> RiskAssessment:
-        """
-        Retrieves the risk assessment results for the dataset using provided configurations.
-
-        Args:
-            json_config (dict): Configuration data for risk assessment.
-
-        Returns:
-            RiskAssessment: A RiskAssessment object containing the results of the analysis.
-        """
-        # Fetch dataset content
         dataset = get_dataset_content(self.dataset_id, self.userid, self.tenantid)
-
-        # Perform risk analysis
-        risk_assessment_result = self.perform_risk_analysis(dataset, json_config)
-
-        return risk_assessment_result
-
-    def perform_risk_analysis(self, dataset: pd.DataFrame, json_config: dict) -> RiskAssessment:
-        """
-        Executes the risk analysis process for a given dataset and configuration.
-
-        Args:
-            dataset (pd.DataFrame): The dataset to be analyzed.
-            json_config (dict): Configuration data for risk assessment.
-
-        Returns:
-            RiskAssessment: A RiskAssessment object encapsulating the results.
-        """
-        # Build the ARX configuration
-        arx_config = self.populate_config_from_dict(json_config)
-
-        # Link dataset to the configuration
-        arx_config = set_data_as_csv(arx_config, dataset)
-
-        # Apply the risk assessment
-        risk_assessment_result = apply_assessment(self.arx_client, arx_config)
-
+        risk_assessment_result = perform_risk_analysis(dataset, json_config, self.arx_client)
         return RiskAssessment(
             userid=self.userid,
             dataset_id=self.dataset_id,
