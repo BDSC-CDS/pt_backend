bbf3da276a5e3d2b9afc9a0146e8d1863f45ca2a
bkaabachi <mohamed-beyrem.kaabachi@chuv.ch>
2024-12-16 15:23:25 +0000

    correct dataset in risk assessment

--- src/pkg/risk_assessment/service/risk_assessment.py
+++ src/pkg/risk_assessment/service/risk_assessment.py
@@ -10,7 +10,7 @@ class RiskAssessmentService:
     def get_risk_assessment(self,  dataset_id: int, tenantid: int, userid: int, json_config: dict) -> RiskAssessment:
         columns_as_lists, total_rows = self.dataset_service.get_dataset_content(dataset_id, userid, tenantid)
         dataset = pd.DataFrame(columns_as_lists).transpose()
-        dataset.columns = [f"Column_{i}" for i in range(df.shape[1])]
+        dataset.columns = [f"Column_{i}" for i in range(dataset.shape[1])]
         initial_highest_prosecutor, initial_average_prosecutor, quasi_identifiers, risk_assessment_server_answer = self.arx_client.perform_risk_analysis(dataset, json_config)
         return RiskAssessment(
             userid=self.userid,
