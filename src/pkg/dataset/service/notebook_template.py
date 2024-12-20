b3c07509eb26a41d97dc10b0b53a69c0be61509c
Sami <sami.perrin@chuv.ch>
2024-12-20 17:41:23 +0100

    update template

--- src/pkg/dataset/service/notebook_template.py
+++ src/pkg/dataset/service/notebook_template.py
@@ -1,8 +1,12 @@
 # %% [markdown]
-# This is a markdown cell
+# # De-identification using the ARX service template
+# 
+# This notebook is a template to perform de-identification using the ARX service.
+
+# %% [markdown]
+# ### Imports 
 
 # %% 
-# imports
 import pandas as pd
 import requests
 from py_arx.assessment_client import apply_assessment
@@ -18,31 +22,38 @@ from py_arx.configuration_client import (
     set_average_prosecutor_risk,
 )
 
+# %% [markdown]
+# ### Parameters
+
 # %%
-# parameters
 token = "{{token}}"
 arx_service_url = "{{arx_service_url}}"
 backend_host = "{{backend_public_url}}"
 
-### Alternatively to get the token you can authenticate
+# Alternatively to get the token you can authenticate
 # username = "{{username}}"
 # password = "<your password here>"
 # response = requests.post(backend_host + "/api/rest/v1/authentication/login", json={"username":username, "password":password})
 # token = response.json()["result"]["token"]
 
+# %% [markdown]
+# ### Fill the dataset
 
 # %%
-############### Build the basic configuration ################
-
-config = build_basic_configuration()
-
-################ Fill the dataset ################
-
 dataset = pd.read_parquet(
     backend_host + "/api/v1/dataset/dataframe/{{datasetid}}", 
     storage_options={"Authorization":"Bearer " + token}
 )
 dataset = dataset.astype(str)
+dataset
+
+# %% [markdown]
+# ### configuration
+
+# %%
+############### Build the basic configuration ################
+
+config = build_basic_configuration()
 config = set_data_as_csv(config, dataset)
 
 ################ Fill the job parameters values ################
@@ -75,8 +86,11 @@ config = configure_attribute(
 #     config, "SEXE", pd.read_csv("../gender_hierarchy.csv", header=None, sep=";")
 # )
 
-################ Execute de-identification by Marketer Risk Level ################
+# %% [markdown]
+# ### Execute de-identification
 
+# %%
+################ Execute de-identification by Marketer Risk Level ################
 config = set_average_prosecutor_risk(config, 0.45)
 
 # %%
