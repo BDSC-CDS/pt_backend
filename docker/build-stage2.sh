5d0c979e694736801a2d0f80efa8193b587ca101
Sami <sami.perrin@chuv.ch>
2025-01-29 19:29:48 +0100

    publish to github only releases and master

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -6,8 +6,10 @@ docker build --pull -f dockerfiles/stage2.dockerfile -t registry.rdeid.unil.ch/p
 
 docker push registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG}
 
-docker tag registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} ghcr.io/bdsc-cds/pt-backend:${IMAGE_TAG}
-docker push ghcr.io/bdsc-cds/pt-backend:${IMAGE_TAG}
+if [[ ${IMAGE_TAG} =~ ^release/v[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    docker tag registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} ghcr.io/bdsc-cds/pt-backend:${IMAGE_TAG}
+    docker push ghcr.io/bdsc-cds/pt-backend:${IMAGE_TAG}
+fi
 
 # if IMAGE_TAG matches master-[0-9]*, push to tag latest
 if [[ ${IMAGE_TAG} =~ ^master-[0-9]+$ ]]; then
