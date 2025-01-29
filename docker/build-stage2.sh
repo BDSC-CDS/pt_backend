8f64c418efd02bf2aeb7ca74efec0cf41ef1f8e8
Sami <sami.perrin@chuv.ch>
2025-01-29 13:26:55 +0100

    add push to ghcr.io

--- docker/build-stage2.sh
+++ docker/build-stage2.sh
@@ -3,6 +3,17 @@
 set -e
 
 docker build --pull -f dockerfiles/stage2.dockerfile -t registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} ..
-docker tag registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} registry.rdeid.unil.ch/pt-backend:latest
+
 docker push registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG}
-docker push registry.rdeid.unil.ch/pt-backend:latest
+
+docker tag registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} ghcr.io/bdsc-cds/pt-backend:${IMAGE_TAG}
+docker push ghcr.io/bdsc-cds/pt-backend:${IMAGE_TAG}
+
+# if IMAGE_TAG matches master-[0-9]*, push to tag latest
+if [[ ${IMAGE_TAG} =~ ^master-[0-9]+$ ]]; then
+  docker tag registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} registry.rdeid.unil.ch/pt-backend:latest
+  docker push registry.rdeid.unil.ch/pt-backend:latest
+  
+  docker tag registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG} ghcr.io/bdsc-cds/pt-backend:latest
+  docker push ghcr.io/bdsc-cds/pt-backend:latest
+fi
