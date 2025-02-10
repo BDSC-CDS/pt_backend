e7a4db72d5244c76698a670a2d613f5a5c660d2b
Sami <sami.perrin@chuv.ch>
2025-02-10 11:28:29 +0100

    align template and deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -8,7 +8,7 @@ cp -r ../configs/$env/* ./chart/files/
 
 helm version
 CONFIG_AES_PASSPHRASE_VAR_NAME="CONFIG_AES_PASSPHRASE_$env"
-helm template --namespace "$env" --values ./chart/files/values.yaml --set-string "aesPassphrase=${!CONFIG_AES_PASSPHRASE_VAR_NAME}" --set-string "image=registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG}" ./chart
+helm template --namespace "$env" --values ./chart/files/values.yaml --set-string "aesPassphrase=${!CONFIG_AES_PASSPHRASE_VAR_NAME}" --set-string "version=$version" --set-string "image=registry.rdeid.unil.ch/pt-backend:${IMAGE_TAG}" ./chart
 
 echo ""
 echo "deploying..."
