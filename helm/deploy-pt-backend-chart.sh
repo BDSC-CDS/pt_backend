423fefb82bf5a9db8ffdeb3d5cac867cee3460f2
Jérémie Frei <jeremie.frei@chuv.ch>
2025-05-15 09:41:56 +0200

    Add dependency update step

--- helm/deploy-pt-backend-chart.sh
+++ helm/deploy-pt-backend-chart.sh
@@ -5,20 +5,26 @@ set -e
 echo "######################################"
 echo "Helm Version:"
 helm version
-echo "\n"
+echo
+
+echo "######################################"
+echo "Helm Dependency Update:"
+helm dependency update ./pt-backend-chart
+helm dependency update ./pt-backend-chart/charts/pt-arx-service-chart
+echo
 
 echo "######################################"
 echo "Helm Lint:"
 helm lint ./pt-backend-chart
-echo "\n"
+echo
 
-# echo "######################################"
-# echo "Helm Template:"
-# helm template ./pt-backend-chart
-# echo "\n"
+echo "######################################"
+echo "Helm Template:"
+helm template ./pt-backend-chart
+echo
 
 echo "######################################"
 echo "Installing or upgrading chart..."
 helm upgrade --install --namespace "test" --create-namespace "pt-backend-test" ./pt-backend-chart
 echo "done"
-echo "\n"
+echo
