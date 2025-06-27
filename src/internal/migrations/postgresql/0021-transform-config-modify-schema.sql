dcce697078a0730547a0964d5774354ea12fbd22
Jérémie Frei <jeremie.frei@chuv.ch>
2025-06-27 12:10:54 +0000

    Fix migrations to create only not null subconfigs

--- src/internal/migrations/postgresql/0021-transform-config-modify-schema.sql
+++ src/internal/migrations/postgresql/0021-transform-config-modify-schema.sql
@@ -1,13 +1,14 @@
 -- Rename table
-ALTER TABLE config_generator RENAME TO transform_config;
+ALTER TABLE IF EXISTS config_generator RENAME TO transform_config;
 
 -- Alter columns
-ALTER TABLE transform_config RENAME COLUMN config_name TO name;
-ALTER TABLE transform_config RENAME COLUMN created_at TO createdat;
-ALTER TABLE transform_config RENAME COLUMN deleted_at TO deletedat;
-ALTER TABLE transform_config ADD COLUMN updatedat TIMESTAMP WITHOUT TIME ZONE;
+ALTER TABLE IF EXISTS transform_config RENAME COLUMN config_name TO name;
+ALTER TABLE IF EXISTS transform_config RENAME COLUMN created_at TO createdat;
+ALTER TABLE IF EXISTS transform_config RENAME COLUMN deleted_at TO deletedat;
+ALTER TABLE IF EXISTS transform_config ADD COLUMN updatedat TIMESTAMP WITHOUT TIME ZONE;
 UPDATE transform_config SET updatedat = createdat;
 ALTER TABLE transform_config ALTER COLUMN updatedat SET NOT NULL;
+ALTER TABLE transform_config ALTER COLUMN updatedat SET DEFAULT now();
 
 -- Create sub tables
 CREATE TABLE IF NOT EXISTS transform_config_date_shift (
@@ -22,7 +23,7 @@ CREATE TABLE IF NOT EXISTS transform_config_date_shift (
     CONSTRAINT transform_config_date_shift_fk FOREIGN KEY (config_id) REFERENCES transform_config(id)
 );
 
-CREATE TABLE transform_config_scramble_fields (
+CREATE TABLE IF NOT EXISTS transform_config_scramble_fields (
     id SERIAL PRIMARY KEY,
     config_id INTEGER NOT NULL,
     field TEXT NOT NULL,
@@ -33,7 +34,7 @@ CREATE TABLE transform_config_scramble_fields (
     CONSTRAINT transform_config_scramble_fields_fk FOREIGN KEY (config_id) REFERENCES transform_config(id)
 );
 
-CREATE TABLE transform_config_sub_field_list (
+CREATE TABLE IF NOT EXISTS transform_config_sub_field_list (
     id SERIAL PRIMARY KEY,
     config_id INTEGER NOT NULL,
     name TEXT NOT NULL,
@@ -47,7 +48,7 @@ CREATE TABLE transform_config_sub_field_list (
     CONSTRAINT transform_config_sub_field_list_fk FOREIGN KEY (config_id) REFERENCES transform_config(id)
 );
 
-CREATE TABLE transform_config_sub_field_regex (
+CREATE TABLE IF NOT EXISTS transform_config_sub_field_regex (
     id SERIAL PRIMARY KEY,
     config_id INTEGER NOT NULL,
     name TEXT NOT NULL,
@@ -65,35 +66,44 @@ CREATE TABLE transform_config_sub_field_regex (
 INSERT INTO transform_config_date_shift (config_id, lowrange, highrange, createdat, updatedat)
 SELECT id, dateShift_lowrange, dateShift_highrange, createdat, updatedat
 FROM transform_config
-WHERE hasDateShift = TRUE;
+WHERE hasDateShift = TRUE
+    AND dateShift_lowrange IS NOT NULL
+    AND dateShift_highrange IS NOT NULL;
 
 INSERT INTO transform_config_scramble_fields (config_id, field, createdat, updatedat)
 SELECT id, UNNEST(string_to_array(scrambleField_fields, ',')) AS field, createdat, updatedat
 FROM transform_config
-WHERE hasScrambleField = TRUE;
+WHERE hasScrambleField = TRUE
+    AND scrambleField_fields IS NOT NULL;
 
 INSERT INTO transform_config_sub_field_list (config_id, name, field, substitute_list, replacement, createdat, updatedat)
 SELECT id, 'subfieldlist', subfieldlist_field, subfieldlist_substitute, subfieldlist_replacement, createdat, updatedat
 FROM transform_config
-WHERE hassubfieldlist = TRUE;
+WHERE hassubfieldlist = TRUE
+    AND subfieldlist_field IS NOT NULL
+    AND subfieldlist_substitute IS NOT NULL
+    AND subfieldlist_replacement IS NOT NULL;
 
 INSERT INTO transform_config_sub_field_regex (config_id, name, field, regex, replacement, createdat, updatedat)
 SELECT id, 'subfieldregex', subfieldregex_field, subfieldregex_regex, subfieldregex_replacement, createdat, updatedat
 FROM transform_config
-WHERE hassubfieldregex = TRUE;
+WHERE hassubfieldregex = TRUE
+    AND subfieldregex_field IS NOT NULL
+    AND subfieldregex_regex IS NOT NULL
+    AND subfieldregex_replacement IS NOT NULL;
 
 -- Drop old columns
 ALTER TABLE transform_config
-DROP COLUMN hasScrambleField,
-DROP COLUMN hasDateShift,
-DROP COLUMN hassubfieldlist,
-DROP COLUMN hassubfieldregex,
-DROP COLUMN scrambleField_fields,
-DROP COLUMN dateShift_lowrange,
-DROP COLUMN dateShift_highrange,
-DROP COLUMN subfieldlist_field,
-DROP COLUMN subfieldlist_substitute,
-DROP COLUMN subfieldlist_replacement,
-DROP COLUMN subfieldregex_field,
-DROP COLUMN subfieldregex_regex,
-DROP COLUMN subfieldregex_replacement;
+DROP COLUMN IF EXISTS hasScrambleField,
+DROP COLUMN IF EXISTS hasDateShift,
+DROP COLUMN IF EXISTS hassubfieldlist,
+DROP COLUMN IF EXISTS hassubfieldregex,
+DROP COLUMN IF EXISTS scrambleField_fields,
+DROP COLUMN IF EXISTS dateShift_lowrange,
+DROP COLUMN IF EXISTS dateShift_highrange,
+DROP COLUMN IF EXISTS subfieldlist_field,
+DROP COLUMN IF EXISTS subfieldlist_substitute,
+DROP COLUMN IF EXISTS subfieldlist_replacement,
+DROP COLUMN IF EXISTS subfieldregex_field,
+DROP COLUMN IF EXISTS subfieldregex_regex,
+DROP COLUMN IF EXISTS subfieldregex_replacement;
